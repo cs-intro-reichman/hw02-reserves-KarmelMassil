9540fe72b48714abc05a9ef1c5a2a9cdc70d73ba
KarmelMassil <karmelreuben.massil@post.runi.ac.il>
2024-01-30 02:01:48 +0200

    space

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -9,11 +9,10 @@ public class DamkaBoard {
 				if (k == 0 && i % 2 == 0) {
 				    System.out.print(" * ");
 				}
-				else {
-					if (k = n)
-				        System.out.print("*");
-				    else
-						System.out.print("* ");
+				else if(k == n && i % 2 == 0)
+				    System.out.print("*");
+				else
+					System.out.print("* ");
 				}
 			}
 			System.out.println("");
