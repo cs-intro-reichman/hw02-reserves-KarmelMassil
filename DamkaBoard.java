67685ecf3b3bebeb69d0412c3d6cb3b38fc78fa4
KarmelMassil <karmelreuben.massil@post.runi.ac.il>
2024-01-30 02:13:20 +0200

    n-1

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,10 +6,9 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 1; i < n+1; i++) { 
 			for (int k = 0; k < n; k++) { 
-				if (k == 0 && i % 2 == 0) {
+				if (k == 0 && i % 2 == 0)
 				    System.out.print(" * ");
-				}
-				else if(k == n && i % 2 == 0)
+				else if(k == n - 1 && i % 2 == 0)
 				    System.out.print("*");
 				else
 					System.out.print("* ");
